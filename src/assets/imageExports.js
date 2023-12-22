4bf001596369a33f12d80689ac86a406fd509908
thomaslynn132 <132193278+thomaslynn132@users.noreply.github.com>
2023-12-22 12:17:32 +0630

    History Page Content and Background Change

---
+++ src/assets/imageExports.js
@@ -0,0 +1,29 @@
+import ygnMdy1 from "./ygn-mdy1.jpg";
+import ygnMdy2 from "./ygn-mdy2.jpg";
+import ygnMdy3 from "./ygn-mdy3.jpg";
+import ygnMdy4 from "./ygn-mdy4.jpg";
+import ygnMdy5 from "./ygn-mdy5.jpg";
+import ygnMdy6 from "./ygn-mdy6.jpg";
+import ygnMdy7 from "./ygn-mdy7.jpg";
+import twtrcr from "./twtrcr.jpg";
+import twtrcr1 from "./twtrcr1.jpg";
+import twtrcr2 from "./twtrcr2.jpg";
+import twtrcr3 from "./twtrcr3.jpg";
+import twtrcr4 from "./twtrcr4.jpg";
+import twtrcr5 from "./twtrcr5.jpg";
+
+export {
+  twtrcr,
+  twtrcr1,
+  twtrcr2,
+  twtrcr3,
+  twtrcr4,
+  twtrcr5,
+  ygnMdy1,
+  ygnMdy2,
+  ygnMdy3,
+  ygnMdy4,
+  ygnMdy5,
+  ygnMdy6,
+  ygnMdy7,
+};
