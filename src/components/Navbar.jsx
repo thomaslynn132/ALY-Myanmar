c467024b5bf75a8fb6f1df43b1e7d9ae3dc971c3
thomaslynn132 <132193278+thomaslynn132@users.noreply.github.com>
2023-12-22 21:04:11 +0630

    mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,41 @@ import Logo from "../assets/Logo.png";
 
 const Navbar = () => {
   const navLinkStyles = ({ isActive }) => {
-    return {
+    const baseStyles = {
       fontWeight: isActive ? "bold" : "small",
       textDecoration: isActive ? "none" : "underline",
       border: isActive ? "none" : "double",
-      height: "25px",
+      height: "20px",
       width: "65px",
       textAlign: "center",
       padding: "5px",
       marginLeft: "5px",
-      color: "Black",
-      fontSize: "17px",
+      color: "black",
+      fontSize: "15px",
       borderRadius: "8%",
       boxShadow: "3px 3px 0px 0px",
       display: "inline-block",
+    };
+
+    const mobileStyles = {
+      fontWeight: isActive ? "bold" : "small",
+      textDecoration: isActive ? "none" : "underline",
+      width: "55px",
+      borderRadius: "8%",
+      textAlign: "center",
+      marginLeft: "5px",
+      display: "block",
+      fontSize: "5px",
+    };
+
+    return {
+      ...baseStyles,
+      "@media screen and (min-width: 767px)": {
+        ...baseStyles,
+      },
+      "@media screen and (max-width: 768px)": {
+        ...mobileStyles,
+      },
     };
   };
 
@@ -38,22 +59,34 @@ const Navbar = () => {
           position: "relative",
           display: "flex",
           flexDirection: "row",
-          justifyContent: 'center',
+          justifyContent: "center",
           width: "100%",
           height: "100px",
           backgroundColor: "skyblue",
         }}>
-        <NavLink link="./" to="/" exact style={{ display: 'flex', flexDirection: "row", justifyContent: 'center', alignItems: 'center',}}>
+        <NavLink
+          link="./"
+          to="/"
+          exact
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
           <img
             src={Logo}
             alt="Logo Of ALY Myanmar"
             height={50}
-            width={50}></img>
+            width={50}
+            style={{ marginLeft: "0" }}></img>
           <label style={{ fontSize: "30px", fontWeight: "bold" }}>
             ALY Myanmar
           </label>
         </NavLink>
-        <div className="navItems">
+        <div
+          className="navItems"
+          style={{ marginLeft: "auto", marginRight: "100px" }}>
           {navItems.map((item, index) => (
             <NavLink
               key={index}
